cb0bbf4bb71180192c6ce0460dedf17da7b9c85c
Abdul Rehman1891 <abdurrehman1891@gmail.com>
2022-11-14 16:07:40 +0500

    lab11

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div data-testid="comp" className="App">
+      <h1> Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Task3.js
@@ -0,0 +1,19 @@
+function Fun()
+{
+    return(
+        <div data-testid="task3" className="fun">
+            <ul>
+                <il>
+                    CAR
+                </il>
+                <il>
+                    Tree
+                </il>
+                <il>
+                    Bike
+                </il>
+            </ul>
+        </div>
+)}
+
+export default Fun;
