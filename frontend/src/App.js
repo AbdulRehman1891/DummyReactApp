dae6ea72b73198bce9cc639028f8878b99634eba
AbdulRehman1891 <abdurrehman1891@gmail.com>
2022-11-23 16:02:29 +0500

    Deployed

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div data-testid="comp" className="App">
+      <h1> Hello World</h1>
+      <h1> Abdul Rehman</h1>
+      <h1> 20i-2344</h1>
+    </div>
+  );
+}
+
+export default App;
